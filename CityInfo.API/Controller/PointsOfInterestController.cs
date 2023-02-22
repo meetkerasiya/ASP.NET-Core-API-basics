9cd326361b7936afe1357917c922c4a9955f54ba
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-22 10:47:09 +0530

    Logger added

--- CityInfo.API/Controller/PointsOfInterestController.cs
+++ CityInfo.API/Controller/PointsOfInterestController.cs
@@ -9,17 +9,37 @@ namespace CityInfo.API.Controller
     [Route("api/cities/{cityid}/pointsofinterest")]
     public class PointsOfInterestController : ControllerBase
     {
+        private readonly ILogger<PointsOfInterestController> _logger;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+
         [HttpGet]
         public IActionResult GetPointsOfInterest(int cityId)
         {
-            var city=CitiesDataStore.current.Cities.FirstOrDefault(x=>x.Id==cityId);
-
-            if (city == null)
+            try
             {
-                return NotFound();
-            }
+                throw new Exception("Example exception");
+                var city = CitiesDataStore.current.Cities.FirstOrDefault(x => x.Id == cityId);
 
-            return Ok(city.PointsOfInterest);
+                if (city == null)
+                {
+                    _logger.LogInformation($"City with id {cityId} wasn't found when accessing point of interest.");
+                    return NotFound();
+                }
+
+                return Ok(city.PointsOfInterest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for ciry with id {cityId}", ex);
+                return StatusCode(500, "A Problem happened while handling your request.");
+            }
+            
         }
 
         [HttpGet("{id}",Name ="GetPointOfInterest")]
